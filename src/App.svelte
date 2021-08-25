d5b1cea03d407a588d400834b1062cb71e5d2dfc
Lê Bá Hoàng An <hoanganleba@gmail.com>
2021-08-25 11:29:08 +0700

    Finished project

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,43 @@
 <script>
-    import {onMount} from "svelte";
-    import TopicCard from "./components/TopicCard.svelte";
+  import { onMount } from "svelte";
+  import TopicCard from "./components/TopicCard.svelte";
+  import { topics, users } from "./store";
 
-    let users;
-    let topics;
-
-    onMount(async () => {
-        fetch('https://forum-proxy.freecodecamp.rocks/latest')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                users = data.users
-                topics = data.topic_list.topics
-            })
-    })
+  onMount(async () => {
+    fetch("https://forum-proxy.freecodecamp.rocks/latest")
+      .then((response) => response.json())
+      .then((data) => {
+        topics.set(data.topic_list.topics);
+        users.set(data.users);
+      });
+  });
 </script>
 
 <main class="container mx-auto md:px-0 px-4 grid gap-y-6">
-    <h1 class="mb-4 py-6 text-center text-xl font-medium bg-green-100 rounded-lg text-green-900">FreeCodeCamp Forum</h1>
-    {#if topics}
-        {#each topics as topic}
-            <TopicCard
-                    title={topic.title}
-                    slug={topic.slug}
-                    id={topic.id}
-                    replies={topic.reply_count}
-                    views={topic.views}
-                    lastActive={topic.last_posted_at}/>
-        {/each}
-    {:else}
-        <p>loading...</p>
-    {/if}
+  <h1
+    class="mb-4 py-6 text-center text-xl font-medium bg-green-100 rounded-lg text-green-900"
+  >
+    FreeCodeCamp Forum
+  </h1>
+  {#if $topics}
+    {#each $topics as topic}
+      <TopicCard
+        title={topic.title}
+        slug={topic.slug}
+        id={topic.id}
+        replies={topic.reply_count}
+        views={topic.views}
+        lastActive={topic.last_posted_at}
+        posters={topic.posters}
+      />
+    {/each}
+  {:else}
+    <p>loading...</p>
+  {/if}
 </main>
 
 <style lang="postcss" global>
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
--- src/components/TopicCard.svelte
+++ src/components/TopicCard.svelte
@@ -1,16 +1,61 @@
 <script>
-    import moment from "moment";
+  import moment from "moment";
+  import { users } from "../store";
 
-    export let title, lastActive, replies, views, slug, id;
+  const getUsername = (user_id) => {
+    const user = $users.filter((user) => user.id === user_id);
+    console.log(user[0].avatar_template);
+    return user[0].username;
+  };
+
+  const getUserAvatar = (user_id) => {
+    const user = $users.filter((user) => user.id === user_id);
+    if (user[0].avatar_template.match(/((http|https):)(www.)?/)) {
+      return replaceRegAvatar(user[0].avatar_template);
+    }
+    return (
+      "https://sea1.discourse-cdn.com/freecodecamp" +
+      replaceRegAvatar(user[0].avatar_template)
+    );
+  };
+
+  const replaceRegAvatar = (url) => {
+    return url.replace(/{(.*?)}/, 40);
+  };
+
+  export let title, lastActive, replies, views, slug, id, posters;
 </script>
 
-
-<div class="p-6 border border-gray-400 rounded-lg">
-    <a href={"https://forum.freecodecamp.org/t/" + slug + "/" + id} target="_blank"
-       class="font-semibold visited:text-black">{title}</a>
+<div
+  class="p-6 border border-gray-400 rounded-lg flex justify-between items-center"
+>
+  <div>
+    <a
+      href={"https://forum.freecodecamp.org/t/" + slug + "/" + id}
+      target="_blank"
+      class="font-semibold visited:text-black">{title}</a
+    >
     <div class="flex mt-2">
-        <div class="text-xs font-medium">Replies: {replies} | Views: {views} | Last
-            Active: {moment(lastActive).startOf('second').fromNow()}</div>
+      <div class="text-xs font-medium">
+        Replies: {replies} | Views: {views} | Last Active: {moment(lastActive)
+          .startOf("second")
+          .fromNow()}
+      </div>
     </div>
+  </div>
+  <div class="flex -space-x-2 overflow-hidden">
+    {#each posters as poster}
+      <a
+        class="font-semibold visited:text-black"
+        href={"https://forum.freecodecamp.org/u/" + getUsername(poster.user_id)}
+        target="_blank"
+      >
+        <img
+          class="inline-block rounded-full ring-2 ring-white"
+          src={getUserAvatar(poster.user_id)}
+          alt={getUsername(poster.user_id)}
+        />
+      </a>
+    {/each}
+  </div>
 </div>
-
